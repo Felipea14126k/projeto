276e505fcad287b18fad00b1bdd094d81ea9c0e9
felipea14126k <gu.souz1309@gmail.com>
2025-06-04 22:11:32 -0300

    sim

--- scripit.js
+++ scripit.js
@@ -3,26 +3,40 @@ const bandName = document.getElementById('band-name');
 const song = document.getElementById('audio');
 const cover = document.getElementById('cover');
 const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
 
-const prayForMe = {
-    songName: 'Pray For Me',
-    artist: 'The Weeknd, Kendrick Lamar',
-    file: 'Kendrick_Lamar_The_Weeknd_-_Pray_For_Me'
+const engenharia = {
+  songName: 'A egenharia',
+  artist: 'The Weeknd, Kendrick Lamar',
+    file: 'engenharia'
+    
 };
 
-const conLaBrisa = {
-    songName: 'Con La Brisa (From Black Panther Wakanda Forever)',
-    artist: 'Foudeqush, Ludwig Göransson',
-    file: 'Con_La_Brisa_From_Black_Panther_Wakanda_Forever'
+const Politica = {
+  songName: 'Politica',
+  artist: 'Kendrick Lamar, SZA',
+ file: 'politica'
 };
-const allTheStar = {
-    songName: 'All The Stars (feat. SZA)',
-    artist: 'Kendrick Lamar, SZA',
-    file: 'All_The_Stars'
+
+//const ProgaramaçãoEcriaçãoDejogos = {
+  //songName: 'Progaramação',
+//artist: 'Gabriel',
+ // file: 'All_The_Stars'
+  //file: 'Programação'
+//};
+
+const explicaçãoProjeto = {
+   songName: 'A Explicação do Projeto',
+ artist: 'Felipe',
+ file: 'explicação do projeto'
 };
+
 
 let isPlaying = false;
-const playlist = [prayForMe, conLaBrisa,allTheStar ];
+const playlist = [engenharia, Politica,explicaçãoProjeto];
 let index = 0;
 
 function playSong() {
@@ -42,20 +56,55 @@ function pauseSong() {
 function playPauseDecider() {
     if (isPlaying === true) {
         pauseSong();
-    } 
+    }
     else {
         playSong();
     }
 }
 
 function initializeSong() {
-    cover.src = `images/${playlist[index].file}.webp`;
+    cover.src = `imagens/${playlist[index].file}.webp`;
     song.src = `songs/${playlist[index].file}.mp3`;
     songName.innerText = playlist[index].songName;
     bandName.innerText = playlist[index].artist;
 }
 
+function previousSong() {
+    if (index === 0)
+        index = playlist.length - 1;
+    else {
+        index -= 1;
+    }
+    initializeSong();
+    playSong();
+}
+function nextSong() {
+    if (index === playlist.length - 1)
+        index - 0;
+    else {
+        index += 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+function updateProgressBar() {
+    const barWidth = (song.currentTime / song.duration) * 100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+
+function jumpTo(event) {
+    const width = progressContainer.clientWidth;
+    const clickPostion = event.offsetX;
+    const jumpToTime = (clickPostion / width) * song.duration;
+    song.currentTime = jumpToTime;
+}
+
+
 initializeSong();
 
 play.addEventListener('click', playPauseDecider);
-
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
+song.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', jumpTo);
